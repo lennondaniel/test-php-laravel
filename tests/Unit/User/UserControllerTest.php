67969dc2febabed6068cb539e254c4a02d3f2bcb
Lennon Daniel <lennon.v90@gmail.com>
2024-03-20 16:12:57 -0300

    add user controller test

--- tests/Unit/User/UserControllerTest.php
+++ tests/Unit/User/UserControllerTest.php
@@ -43,7 +43,29 @@ class UserControllerTest extends TestCase
         $apiContent = json_decode($apiResponse->getContent());
 
         $this->assertEquals(201, $apiResponse->getStatusCode());
-        $this->assertEquals($request->input('name'), $apiContent->name);
-        $this->assertEquals($request->input('email'), $apiContent->email);
+        $this->assertEquals('success', $apiContent->status);
+        $this->assertEquals($request->input('name'), $apiContent->data->name);
+        $this->assertEquals($request->input('email'), $apiContent->data->email);
+    }
+
+    public function test_create_user_error(): void
+    {
+        $user = User::factory()->make();
+        $request = new Request();
+        $request->merge([
+            'name' => $user['name'],
+            'email' => $user['email'],
+            'password' => 'test'
+        ]);
+        $userModel = new User();
+        $userRepository = new UserRepository($userModel);
+        $userService = new UserService($userRepository);
+        $userController = new UserAuthController($userService) ;
+        $apiResponse = $userController->register($request);
+        $apiContent = json_decode($apiResponse->getContent());
+
+        $this->assertEquals(400, $apiResponse->getStatusCode());
+        $this->assertEquals('error', $apiContent->status);
+        $this->assertObjectHasProperty('message', $apiContent);
     }
 }
