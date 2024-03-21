ba7e1d3ce9f10b6bd22eba77b62ee08fbfaa9a4c
Lennon Daniel <lennon.v90@gmail.com>
2024-03-21 19:39:40 -0300

    change binds to app service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,12 @@
 
 namespace App\Providers;
 
+use App\Repositories\Weather\WeatherRepository;
+use App\Repositories\Weather\WeatherRepositoryInterface;
+use App\Services\OpenWeather\OpenWeatherService;
+use App\Services\OpenWeather\OpenWeatherServiceInterface;
+use App\Services\Weather\WeatherService;
+use App\Services\Weather\WeatherServiceInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +17,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(OpenWeatherServiceInterface::class, OpenWeatherService::class);
+        $this->app->bind(WeatherRepositoryInterface::class, WeatherRepository::class);
+        $this->app->bind(WeatherServiceInterface::class, WeatherService::class);
     }
 
     /**
