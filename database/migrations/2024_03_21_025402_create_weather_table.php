e4f1423b9363f0ad1df32ecb2653688fa9736e07
Lennon Daniel <lennon.v90@gmail.com>
2024-03-21 01:06:56 -0300

    add weather migration

---
+++ database/migrations/2024_03_21_025402_create_weather_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('weather', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('city')->unique();
+            $table->double('lat');
+            $table->double('lon');
+            $table->jsonb('data_weather');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('weather');
+    }
+};
