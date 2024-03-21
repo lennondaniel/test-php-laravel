b4325bcdb2121b892e00b21a76964c05ea48b1df
Lennon Daniel <lennon.v90@gmail.com>
2024-03-21 09:54:04 -0300

    add weather repository test

---
+++ tests/Unit/Weather/WeatherRepositoryTest.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace Tests\Unit\Weather;
+
+use App\DTOs\User\UserDTO;
+use App\DTOs\Weather\WeatherDTO;
+use App\Models\Weather;
+use App\Repositories\User\UserRepository;
+use App\Repositories\Weather\WeatherRepository;
+use Database\Factories\WeatherFactory;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Validation\ValidationException;
+use Tests\TestCase;
+use WendellAdriel\ValidatedDTO\Exceptions\CastTargetException;
+use WendellAdriel\ValidatedDTO\Exceptions\MissingCastTypeException;
+
+class WeatherRepositoryTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    public function tearDown(): void
+    {
+        parent::tearDown();
+    }
+
+    /**
+     * @return void
+     */
+    public function test_get_all_weather_repository(): void
+    {
+        $weatherFactory = Weather::factory(3)->create();
+
+        $weatherRepository = new WeatherRepository(new Weather());
+        $weathers = $weatherRepository->getAll();
+
+        $this->assertInstanceOf(Collection::class, $weathers);
+        $this->assertEquals(3, $weathers->count());
+    }
+
+    /**
+     * @return void
+     */
+    public function test_get_all_with_filter_city_weather_repository(): void
+    {
+        $weatherFactory = Weather::factory(3)->create();
+        $weatherArray = $weatherFactory->toArray();
+        $weatherRepository = new WeatherRepository(new Weather());
+        $weathers = $weatherRepository->getAll($weatherArray[0]['city']);
+
+        $this->assertInstanceOf(Builder::class, $weathers);
+        $this->assertEquals(1, $weathers->count());
+    }
+
+    /**
+     * @return void
+     * @throws CastTargetException
+     * @throws MissingCastTypeException
+     * @throws ValidationException
+     */
+    public function test_create_weather_repository(): void
+    {
+        $weather = Weather::factory()->make();
+        $weatherDTO = new WeatherDTO($weather->toArray());
+
+        $weatherRepository = new WeatherRepository(new Weather());
+        $weatherCreated = $weatherRepository->create($weatherDTO);
+
+        $this->assertInstanceOf(Weather::class, $weatherCreated);
+        $this->assertEquals($weatherDTO->city, $weatherCreated['city']);
+    }
+
+    /**
+     * @return void
+     */
+    public function test_show_weather_repository(): void
+    {
+        $weatherFactory = Weather::factory(3)->create();
+        $weatherArray = $weatherFactory->toArray();
+        $weatherRepository = new WeatherRepository(new Weather());
+        $weather = $weatherRepository->show($weatherArray[0]['id']);
+        $weatherCollect = $weather->toArray();
+
+        $this->assertInstanceOf(Weather::class, $weather);
+        $this->assertEquals($weatherArray[0]['id'], $weatherCollect['id']);
+    }
+
+    /**
+     * @throws CastTargetException
+     * @throws MissingCastTypeException
+     * @throws ValidationException
+     */
+    public function test_update_success_weather_repository(): void
+    {
+        $weatherFactory = Weather::factory()->create();
+        $weatherDTO = new WeatherDTO($weatherFactory->toArray());
+        $weatherRepository = new WeatherRepository(new Weather());
+        $weather = $weatherRepository->update($weatherFactory['id'], $weatherDTO);
+
+        $this->assertTrue($weather);
+    }
+
+    /**
+     * @return void
+     */
+    public function test_delete_weather_repository(): void
+    {
+        $weatherFactory = Weather::factory()->create();
+        $weatherRepository = new WeatherRepository(new Weather());
+        $weather = $weatherRepository->delete($weatherFactory['id']);
+
+        $this->assertTrue($weather);
+    }
+}
